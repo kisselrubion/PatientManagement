39e0e9d682d787c922cdae56020bbe8d8fcaac59
kissel rubion <kisselrubion@outlook.ph>
2019-12-09 17:06:12 +0800

    improved ui in course page

--- WristCare/WristCare/RequestProvider/RequestProvider.cs
+++ WristCare/WristCare/RequestProvider/RequestProvider.cs
@@ -172,10 +172,10 @@ namespace WristCare.RequestProvider
 			{
 				//android base ip 10.0.2.2
 				//this ip changes every once in a while so always check
-				BaseAddress = new Uri("http://192.168.1.2/pms/api/")
+				//BaseAddress = new Uri("http://192.168.1.2/pms/api/")
 
 				//office
-				//BaseAddress = new Uri("http://192.168.1.59/pms/api/")
+				BaseAddress = new Uri("http://192.168.1.59/pms/api/")
 				
 				//local
 				//BaseAddress = new Uri("http://localhost/pms/api/")
